66538fedd6220e75fccacf3057781a6de0d5b2c9
Rikki Kendall <yrk3@duke.edu>
2019-11-11 20:31:28 -0500

    Simple NavBar implemented

---
+++ athlete-star-starter/src/NavBar.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+
+// material-ui
+import { withStyles } from '@material-ui/core/styles'
+import { Grid, Button, AppBar, SwipeableDrawer, IconButton, List, Divider, ListItem, ListItemIcon, ListItemText} from '@material-ui/core/';
+import { MailIcon, AccountCircle } from '@material-ui/icons/';
+import PropTypes from 'prop-types';
+import MenuIcon from '@material-ui/icons/Menu/';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
+
+class NavBar extends Component {
+    state = {
+    open: false,
+   };
+
+   //Opening Side Handler
+   handleDrawerOpen = () => {
+     this.setState({ open: true });
+   };
+
+   //Closing Side Handler
+    handleDrawerClose = () => {
+      this.setState({ open: false });
+    };
+
+  render() {
+    const { user, classes, logout } = this.props;
+    return (
+      <div>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+            </IconButton>
+            <Typography variant="h6">
+            NCAA Athlete Brand Network
+            </Typography>
+            <Button color="inherit">Login</Button>
+            </Toolbar>
+          </AppBar>
+        </div>
+    );
+  }
+}
+
+NavBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+//export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(Nav));
+export default (NavBar);
+
+export {
+  NavBar,
+};
