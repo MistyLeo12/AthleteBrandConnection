9a5dd04dcdbeffc1aad0b3aa3bbb0ec82690f0a0
Akshay Mehta <akshaydm33@gmail.com>
2019-12-04 18:24:56 -0500

    review page

--- athlete-star-starter/src/App.js
+++ athlete-star-starter/src/App.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import { NavBar } from './NavBar';
-import { Cards } from './Cards';
-import { MultipleItems } from './MultipleItems';
-import { CustomArrows } from './CustomArrows';
-import { PreviousNextMethods } from './PreviousNextMethods';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import { NavBar } from "./NavBar";
+import { Cards } from "./Cards";
+import { MultipleItems } from "./MultipleItems";
+import { CustomArrows } from "./CustomArrows";
+import { PreviousNextMethods } from "./PreviousNextMethods";
+import { Reviews } from "./Reviews.jsx";
+import "./App.css";
 
-
-export default class App extends Component <Props> {
-  render () {
+export default class App extends Component<Props> {
+  render() {
     return (
       <div className="App">
         <NavBar />
         <br />
-        <CustomArrows />
-        <br />
-        <PreviousNextMethods />
-        
+        <Reviews />
+
         <br />
       </div>
     );
