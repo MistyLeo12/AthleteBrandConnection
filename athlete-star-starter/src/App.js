30d1a3b425cc18572282de6f95d406b874131fa8
johnwferry <58151498+johnwferry@users.noreply.github.com>
2019-12-08 10:06:22 -0500

    User auth working
    auth working, not pretty: need to work into existing structure

--- athlete-star-starter/src/App.js
+++ athlete-star-starter/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import firebase from "firebase";
-import { StyledFirebaseAuth } from "react-firebaseui/StyledFirebaseAuth";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import { NavBar } from './NavBar';
 import { Cards } from './Cards';
 import { MultipleItems } from './MultipleItems';
@@ -10,7 +10,32 @@ import { PreviousNextMethods } from './PreviousNextMethods';
 import './App.css';
 
 
+firebase.initializeApp({
+  apiKey: "AIzaSyDy1LlgOmltOLOjfsisTBWuKH9XrCfWMa4",
+  authDomain: "athlete-market.firebaseapp.com"
+})
+
 export default class App extends Component <Props> {
+  state={isSignedIn: false }
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  }
+
+  componentDidMount = () => {
+
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({isSignedIn:!!user})
+      console.log("user", user)
+    })
+  }
   render () {
     return (
       <div className="App">
@@ -20,6 +45,21 @@ export default class App extends Component <Props> {
         <br />
         <PreviousNextMethods />
         <br />
+        {this.state.isSignedIn ?
+          <span>
+            <div> Signed In </div>
+            <button onClick={() => firebase.auth().signOut()}>Sign out</button>
+            <h1>Welcome {firebase.auth().currentUser.displayname}</h1>
+            <img alt="profile pic" src={firebase.auth().currentUser.photoURL} />
+          </span>
+          :
+        (
+        <StyledFirebaseAuth
+          uiConfig={this.uiConfig}
+          firebaseAuth={firebase.auth()}
+          />
+        )
+      }
       </div>
     );
   }
