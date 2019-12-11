23a8eb18064574bd5a169f8a2cd30e04a4f70887
Robert Koech <rk214@duke.edu>
2019-12-11 01:44:09 -0500

    update

--- athlete-star-starter/src/PreviousNextMethods.jsx
+++ athlete-star-starter/src/PreviousNextMethods.jsx
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
+import image from "./pictures/profiles/HaleyG.png";
+import image2 from "./pictures/profiles/Will.jpg";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { Paper, Image, Typography, Button, Card, CardActions, CardMedia, CardActionArea, CardContent } from '@material-ui/core/';
-import logo from './Tre_Jones.jpg';
+import {
+  Paper,
+  Image,
+  Typography,
+  Button,
+  Card,
+  CardActions,
+  CardMedia,
+  CardActionArea,
+  CardContent
+} from "@material-ui/core/";
+import logo from "./Tre_Jones.jpg";
 import { Link } from "react-router-dom";
-import { db } from "./fire"
+import { db } from "./fire";
 
 class PreviousNextMethods extends Component {
   constructor(props) {
@@ -14,7 +26,7 @@ class PreviousNextMethods extends Component {
     this.previous = this.previous.bind(this);
     this.state = {
       data: [],
-      players: [] 
+      players: []
     };
   }
   componentDidMount() {
@@ -23,12 +35,10 @@ class PreviousNextMethods extends Component {
       .then(querySnapshot => {
         const data = querySnapshot.docs.map(doc => doc.data());
         console.log(data);
-        this.setState({ players: data 
-        });
+        this.setState({ players: data });
       });
-  
   }
-  
+
   next() {
     this.slider.slickNext();
   }
@@ -45,58 +55,88 @@ class PreviousNextMethods extends Component {
     };
     const { players } = this.state;
 
-
     return (
       <div>
         <h2>Basketball</h2>
-   
+
         <Slider ref={c => (this.slider = c)} {...settings}>
-        {players.map(player=>(
-          <div key={0}>
-            <Link to="/athletes">
-            <Paper>
-              <Card className="our-team">
-                <CardActionArea className="picture">
-                  <CardMedia
-                  component="img"
-                  height="300px"
-                  maxWidth="50px"
-                  className = "img-fluid"
-                  image = { logo } 
-                  />
-                   
-                  <CardContent className="team-content">
-                    <Typography className="name" gutterBottom variant="h5" component="h2">
-                    {player.firstName} {player.lastName}
-                    </Typography>
-                    <Typography className="title" variant="body2" color="textSecondary" component="p">
-                    {player.school} {player.sport}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
+          {players.map(player => (
+            <div key={0}>
+              <Link to="/athletes">
+                <Paper>
+                  <Card className="our-team">
+                    <CardActionArea className="picture">
+                      {player.firstName !== "Tre" && (
+                        <CardMedia
+                          component="img"
+                          height="300px"
+                          maxWidth="50px"
+                          className="img-fluid"
+                          image=""
+                        />
+                      )}
+                      {player.firstName === "Haley" && (
+                        <CardMedia
+                          component="img"
+                          height="300px"
+                          maxWidth="50px"
+                          className="img-fluid"
+                          image={image}
+                        />
+                      )}
+                      {player.firstName === "Will" && (
+                        <CardMedia
+                          component="img"
+                          height="300px"
+                          maxWidth="50px"
+                          className="img-fluid"
+                          image={image2}
+                        />
+                      )}
 
-              </Card>
-            </Paper>
-            </Link>
+                      {player.firstName === "Tre" && (
+                        <CardMedia
+                          component="img"
+                          height="300px"
+                          maxWidth="50px"
+                          className="img-fluid"
+                          image={logo}
+                        />
+                      )}
+                      <CardContent className="team-content">
+                        <Typography
+                          className="name"
+                          gutterBottom
+                          variant="h5"
+                          component="h2"
+                        >
+                          {player.firstName} {player.lastName}
+                        </Typography>
+                        <Typography
+                          className="title3"
+                          variant="body2"
+                          color="textSecondary"
+                          component="p"
+                        >
+                          {player.school} {player.sport}
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                  </Card>
+                </Paper>
+              </Link>
             </div>
-            ))}
-          <div key={1}>
-
-          </div>
-          <div key={2}>
-            3
-          </div>
-          <div key={3}>
-            4
-          </div>
+          ))}
+          <div key={1}></div>
+          <div key={2}>3</div>
+          <div key={3}>4</div>
           <div key={4}>
             <h3>5</h3>
           </div>
-    
         </Slider>
         <br />
         <br />
-  
+
         <div style={{ textAlign: "center" }}>
           <button className="button" onClick={this.previous}>
             Previous
@@ -105,7 +145,6 @@ class PreviousNextMethods extends Component {
             Next
           </button>
         </div>
-
       </div>
     );
   }
@@ -113,6 +152,4 @@ class PreviousNextMethods extends Component {
 
 export default PreviousNextMethods;
 
-export {
-  PreviousNextMethods,
-};
+export { PreviousNextMethods };
