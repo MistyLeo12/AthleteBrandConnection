b28c8ffb157400d87ca5a6a3914504432f63b25b
Rikki Kendall <yrk3@duke.edu>
2019-12-11 00:35:25 -0500

    Update NavBar.jsx

--- athlete-star-starter/src/NavBar.jsx
+++ athlete-star-starter/src/NavBar.jsx
@@ -14,20 +14,6 @@ import firebase from "firebase";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import logo from './ncaalogo.png'
 
-
-firebase.initializeApp({
-    apiKey: "AIzaSyDy1LlgOmltOLOjfsisTBWuKH9XrCfWMa4",
-    authDomain: "athlete-market.firebaseapp.com",
-    databaseURL: "https://athlete-market.firebaseio.com",
-    projectId: "athlete-market",
-    storageBucket: "athlete-market.appspot.com",
-    messagingSenderId: "370761758459",
-    appId: "1:370761758459:web:7e7a74d2d292b61ac6a25e",
-    measurementId: "G-QWJLD6YH8F"
-  });
-        
-import logo from './ncaalogo.png'
-
 class NavBar extends Component {
   state={isSignedIn: false }
   uiConfig = {
